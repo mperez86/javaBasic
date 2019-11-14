49315006d9daa53298ef0a4240db27e470be145c
mperez86 <manuelperezcaballero86@gmail.com>
2019-11-14 17:55:44 +0100

    calculadora

--- Calculadora/src/main/Calculadora.java
+++ Calculadora/src/main/Calculadora.java
@@ -2,6 +2,31 @@ package main;
 
 public class Calculadora {
 
+	// multiples valores
+	public int suma(int... numeros) { // --> arreglo
+		System.out.println(numeros.length);
+		int suma = 0;
+		for (int numero : numeros)
+			suma += numero;
+		return suma;
+	}
+	
+	public float suma(float... numeros) { // --> arreglo
+		System.out.println(numeros.length);
+		float suma = 0;
+		for (float numero : numeros)
+			suma += numero;
+		return suma;
+	}
+	
+	public double suma(double... numeros) { // --> arreglo
+		System.out.println(numeros.length);
+		double suma = 0;
+		for (double numero : numeros)
+			suma += numero;
+		return suma;
+	}
+	
 	public int suma(int valor1, int valor2) {
 		return valor1 + valor2;
 	}
